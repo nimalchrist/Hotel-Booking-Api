68c7b55ed080f374c1276e559fb2a7f08482697d
DESKTOP-VCFBM60\DELL <darinsdd2002@gmail.com>
2023-07-26 15:34:16 +0530

    encryption and decryption in utils.js 26/7/2023

--- models/users.model.js
+++ models/users.model.js
@@ -1,11 +1,13 @@
 // TODO the definition of users model
 const mongoose = require("mongoose");
-
+const encryptionUtil = require('../utils/utils');
 const cardSchema = new mongoose.Schema({
   cardNumber: {
     type: String,
     required: true,
     unique: true,
+    set: encryptionUtil.encrypt, // Encrypt the card number before saving
+    get: encryptionUtil.decrypt, // Decrypt the card number when accessed
   },
   cardHolder: {
     type: String,
@@ -18,6 +20,8 @@ const cardSchema = new mongoose.Schema({
   cvv: {
     type: String,
     required: true,
+    set: encryptionUtil.encrypt, // Encrypt the cvv before saving
+    get: encryptionUtil.decrypt, // Decrypt the cvv when accessed
   },
 });
 
--- utils/utils.js
+++ utils/utils.js
@@ -1,1 +1,29 @@
 // encryption decryption files will be there...
+const crypto = require('crypto');
+require("dotenv").config();
+
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+const IV_LENGTH = process.env.IV_LENGTH;
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+}
+
+function decrypt(text) {
+  const parts = text.split(':');
+  const iv = Buffer.from(parts[0], 'hex');
+  const encryptedText = Buffer.from(parts[1], 'hex');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
+
+module.exports = {
+  encrypt,
+  decrypt,
+};

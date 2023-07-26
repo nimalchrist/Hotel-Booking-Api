5f686723b007d29d8c0a92bdc68299408f8e3c42
KANNANPARKER\ACER <therikannan760@gmail.com>
2023-07-26 17:34:48 +0530

    database connection middle ware initilization 26/07/2023

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ const app = express();
 const PORT = process.env.SERVER_PORT;
 
 //middlewares section
+const mongoUri = "mongodb://localhost:27017/auth_db";
 
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+connectToMongoDB();
 //listen section
 app.listen(PORT, (error) => {
   if (error) {

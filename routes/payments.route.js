be69594dcb75570702637a026afb490f5ffcd907
DESKTOP-BP14JQO\Dell <musiselfha@gmail.com>
2023-07-27 17:17:46 +0530

    payment route declatation 27-07-2023

--- routes/payments.route.js
+++ routes/payments.route.js
@@ -1,1 +1,9 @@
 // TODO all the routes corresponding to the payments controller
+const express = require('express');
+const router = express.Router();
+const paymentController = require('../controller/paymentController');
+
+// Route to handle adding a new payment
+router.route("/add").post( paymentController.addPayment);
+
+module.exports = router;

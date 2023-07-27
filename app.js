177eab247fe5300f2d850680754bf4214fd4c60b
DESKTOP-VCFBM60\DELL <darinsdd2002@gmail.com>
2023-07-27 13:00:00 +0530

    middleware updated

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.SERVER_PORT;
+// Import routes
+const usersRoute = require('./routes/users.route');
+
 
 //middlewares section
+app.use(express.json()); // Parse JSON requests
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
+
+// Use the usersRoute for handling user-related endpoints
+app.use("/user", usersRoute);
+
+
 
 //listen section
 app.listen(PORT, (error) => {

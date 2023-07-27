5365009bd0d5a54fcee753122d03b00c2194c8eb
fency <fencyj854@gmail.com>
2023-07-27 17:55:43 +0530

    27/7/23-get delete apis

--- controllers/hotels.controller.js
+++ controllers/hotels.controller.js
@@ -45,6 +45,61 @@ async function handleSearchRequest(req, res) {
   }
 }
 
+
+async function deleteHotelById(req, res) {
+  try {
+    const hotelId = req.params.id;
+    console.log(hotelId);
+    const hotel = await findHotelByIdQuery(hotelId);
+    console.log(hotel);
+    if (!hotel) {
+      return res.status(404).json({ error: "Hotel not found." });
+    }
+
+    await deleteHotelByIdQuery(hotelId);
+
+    res.json({ message: "Hotel deleted successfully." });
+  } catch (error) {
+    console.error("Error deleting hotel:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the hotel." });
+  }
+}
+
+async function getAllHotels(req, res) {
+  try {
+    const hotels = await findAllHotelsQuery();
+    console.log(hotels);
+    res.json(hotels);
+  } catch (error) {
+    console.error("Error fetching hotels:", error);
+    res.status(500).json({ error: "An error occurred while fetching hotels." });
+  }
+}
+
+async function getHotelById(req, res) {
+  try {
+
+    const hotelId = req.params.id;
+    console.log(hotelId);
+    const hotel = await findHotelByIdQuery(hotelId);
+    console.log(hotel);
+    if (!hotel) {
+      return res.status(404).json({ error: "Hotel not found." });
+    }
+
+    console.log(hotel);
+    res.json(hotel);
+  } catch (error) {
+    console.error("Error fetching hotels:", error);
+    res.status(500).json({ error: "An error occurred while fetching hotel." });
+  }
+}
 module.exports = {
   handleSearchRequest,
+  deleteHotelById,
+  getAllHotels,
+  getHotelById,
 };
+
--- services/hotels.service.js
+++ services/hotels.service.js
@@ -1,1 +1,15 @@
 // TODO the services of hotels collection
+const Hotel = require("./model");
+
+exports.findHotelByIdQuery = async (id) => {
+  console.log(Hotel.findOne({ _id: id }));
+  return Hotel.findOne({ _id: id });
+};
+
+exports.deleteHotelByIdQuery = async (id) => {
+  return Hotel.deleteOne({ _id: id });
+};
+
+exports.findAllHotelsQuery = async () => {
+  return Hotel.find();
+};

0ee4ad7cf233167c8c2c8524c9afecbff6f7b321
DESKTOP-BP14JQO\Dell <musiselfha@gmail.com>
2023-07-31 11:07:41 +0530

    user model schema changed to sparse 31-07-2023

--- models/payments.model.js
+++ models/payments.model.js
@@ -1,4 +1,5 @@
 // TODO the definition of payments model
+const mongoose = require('mongoose');
 const paymentSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
--- routes/payments.route.js
+++ routes/payments.route.js
@@ -1,9 +1,9 @@
 // TODO all the routes corresponding to the payments controller
 const express = require('express');
 const router = express.Router();
-const paymentController = require('../controller/paymentController');
+const paymentController = require('../controllers/payments.controller');
 
 // Route to handle adding a new payment
-router.route("/payment/:userid/:hotelId/add").post( payments.controller.Controller);
+router.route("/payment/:userid/:hotelId/add").post( paymentController.Controller);
 
 module.exports = router;

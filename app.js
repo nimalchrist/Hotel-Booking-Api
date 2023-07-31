4df0905adc652d6d72e553b6fe1e972f58212e6a
DESKTOP-BP14JQO\Dell <musiselfha@gmail.com>
2023-07-31 10:40:07 +0530

    before the globe-db connection 31-07-23

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const mongoose=require("mongoose");
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.SERVER_PORT;
+const mongoose = require('mongoose');
+const Routes = require('./routes/payments.route');
+const User = require('./models/users.model');
+const Hotel = require('./models/hotels.model');
 //middlewares section
-mongoose.connect('mongodb://127.0.0.1:27017/hotel_management',
- {
+app.use(express.json());
+
+// Set up payment routes
+app.use('/', Routes);
+//db connection 
+const mongodbUri = 'mongodb+srv://gceInterns:IcanioGCE@gcecluster1.hfhvmqk.mongodb.net/globe_db';
+
+mongoose.connect(mongodbUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 //listen section
 app.listen(PORT, (error) => {
   if (error) {

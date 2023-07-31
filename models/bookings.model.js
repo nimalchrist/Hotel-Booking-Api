620f94b95ceae74e7c35e49fd5279126c011e5d2
harsikasivakumar <harsusivakumar06@gmail.com>
2023-07-31 11:59:14 +0530

    updated booking model

--- models/bookings.model.js
+++ models/bookings.model.js
@@ -1,35 +1,50 @@
 // TODO the definition of bookings model
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
+
   hotelId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "hotels",
+    type:mongoose.Schema.Types.ObjectId ,
+    ref:'hotels',
     required: true,
   },
+
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
+    ref:'users',
     required: true,
   },
+
   paymentId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "payments",
+    ref: 'Payments',
+    required:true,
+  },
+
+  roomId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Hotel.rooms',
     required: true,
   },
-  reservation: {
+
+  
+    reservation: {
+
     checkInDate: {
       type: Date,
       required: true,
     },
+
     checkOutDate: {
       type: Date,
       required: true,
     },
+
     roomType: {
       type: String,
       required: true,
     },
+
     numberOfRooms: {
       type: Number,
       required: true,
@@ -37,5 +52,5 @@ const bookingSchema = new mongoose.Schema({
   },
 });
 
-const bookings = mongoose.model("booking", bookingSchema);
-module.exports = bookings;
+const booking=mongoose.model("book",bookingSchema);
+module.exports=booking;

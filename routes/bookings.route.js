94ba5f01576f4e6d19e4fb859908e853192dafa6
harsikasivakumar <harsusivakumar06@gmail.com>
2023-07-31 10:34:09 +0530

    booking routes

--- routes/bookings.route.js
+++ routes/bookings.route.js
@@ -1,1 +1,8 @@
-// TODO all the routes corresponding to the bookings controller
+const express = require('express');
+const controller=require("../controllers/controller");
+const router = express.Router();
+
+router.route("/booking").post(controller.createBooking);
+router.route("/booking/:hotelId").get(controller.getHotelBookings);
+
+module.exports = router;

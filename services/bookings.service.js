855efddb05f0ea0b7c4910f861b3d6a6d20f33d2
harsikasivakumar <harsusivakumar06@gmail.com>
2023-07-31 10:28:54 +0530

    booking services

--- services/bookings.service.js
+++ services/bookings.service.js
@@ -1,1 +1,20 @@
-// TODO the services of bookings collection
+const Booking = require('../models/booking');
+
+// Function to create a new booking
+
+const createBooking = async (bookingData) => {
+  
+  
+  const newBooking = new Booking(bookingData);
+    const savedBooking = await newBooking.save();
+    return savedBooking;
+};
+
+// Function to get hotel bookings
+
+
+const getHotelBookings = async (hotelId) => {
+
+    const bookings = await Booking.find({ hotelId });
+    return bookings;
+  }

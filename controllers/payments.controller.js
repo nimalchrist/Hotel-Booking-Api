af26ac798d62bd2d9c7fa2bbd5a2189553b32d96
DESKTOP-BP14JQO\Dell <musiselfha@gmail.com>
2023-08-03 10:13:40 +0530

    integrated controller and service in payment 3/8

--- controllers/payments.controller.js
+++ controllers/payments.controller.js
@@ -1,10 +1,17 @@
-// TODO the controllers of payments collection
-const paymentServices = require("../services/payments.service");
+// controllers/payments.controller.js
+const paymentService = require("../services/payments.service");
 
 exports.addPaymentController = async (req, res) => {
   if (req.isAuthenticated()) {
     try {
-      await paymentServices.addPayment(req, res);
+      const userId = req.user;
+      const hotelId = req.params.hotelId;
+      const paymentResult = await paymentService.addPayment(userId, hotelId);
+      if (paymentResult.status === 'success') {
+        return res.status(200).json({ status: paymentResult.paymentStatus });
+      } else {
+        return res.status(404).json({ error: paymentResult.message });
+      }
     } catch (err) {
       console.log(err);
       res.status(500).json({ error: "Internal Server Error" });
